04c2cc2424578eef0dbbddad0bb590a507e44494
Pavel <pavelyeganov@gmail.com>
2018-12-31 13:18:40 -0500

    2nd commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //container for all environments
-var envs = {}
 
-envs.dev = {
+let baseUrl = process.env.NODE_ENV == 'production' ? 'https://fast-journey-14415.herokuapp.com/' : 'http://localhost:4000/';
+let env = {
   httpPort:4000,
   httpsPort:4001,
   envName: 'development',
@@ -10,22 +10,7 @@ envs.dev = {
   templateGlobals: {
     companyName: 'Jinko',
     yearCreated: '2018',
-    baseUrl: 'http://localhost:4000/'
+    baseUrl: baseUrl
   }
 }
-envs.prod = {
-  httpPort:5000,
-  httpsPort:5001,
-  envName: 'production',
-  hashSecret: 'thisisasecret', 
-  templateGlobals: {
-    companyName: 'Jinko',
-    yearCreated: '2018',
-    baseUrl: 'http://localhost:5000/'
-  }
-}
-if(process.env.NODE_ENV==='production'){
-  module.exports = envs.prod;
-} else {
-  module.exports = envs.dev;
-}
+module.exports = env;
