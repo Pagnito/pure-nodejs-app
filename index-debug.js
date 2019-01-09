3d527a61324a6a0cd64b7ffcc96ae3893516296b
Pavel <pavelyeganov@gmail.com>
2019-01-09 15:00:18 -0500

    push haaaaardaa

--- index-debug.js
+++ index-debug.js
@@ -8,8 +8,23 @@ app.init = function(){
 	setTimeout(function(){
 		cli.init();
 	},300)
+
+
+	 g = 1;
+	debugger;
+
+
+	g+=10;
+	debugger;
+
+
+	g=5;
+	debugger;
+	
+	console.log('g son son')
 }
 
 app.init();
+
 
 module.exports = app;
